35a92188f8e6f2f1cfc0968698abf0070cec3452
Jacques Troussard <mm_sparrow@beeswaxnoneofyours.com>
2025-02-15 11:53:09 -0500

    init project files, adds spring project initializer files

---
+++ src/main/java/me/devlife4/backend/WebApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package me.devlife4.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/devlife4/backend/WebApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devlife4.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
